2cc1f01b884c90c5168dd78fdc5c5936ba881a57
ElectriPixie <ElectriPixie@gmail.com>
2025-08-09 21:39:06 -0400

    updated

--- harnessaurus.py
+++ harnessaurus.py
@@ -37,6 +37,7 @@ def main():
                             'hidden_injection_detector.HiddenPromptInjectionDetector',
                             'json_logger.JsonLogger',
                             'refusal_detector.RefusalDetector',
+                            'reward_hacking_detector.RewardHackingDetector'
                         ],
                         help='List of plugins to load with optional params like mod.Class:param=val')
     parser.add_argument('--batch_size', type=int, default=8)
